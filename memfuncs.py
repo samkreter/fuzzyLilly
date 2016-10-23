431e374fba243f1e8495964c4ae54df07e1f897e
Sam Kreter <samkreter@gmail.com>
2016-10-23 15:22:28 -0500

    fixed class compitlation for the membership functions

--- memfuncs.py
+++ memfuncs.py
@@ -4,32 +4,35 @@ import numpy as np
 
 class memFunc():
     """docstring for memFunc"""
+
     def __init__(self, specs=[]):
         length = len(specs)
+        self.specs = specs
+
         if length == 3:
-            self.memFunc = self.trap(specs)
+            self.memberFuncName = "triangle"
+            self.memFunc = self.tri
         elif length == 4:
-            self.memFunc = self.tri(specs)
+            self.memberFuncName = "trapezoid"
+            self.memFunc = self.trap
         else:
             raise Exception("Specs for membership function not correctly passed in")
 
-    def trap(self,specs):
-        pass
+    def __repr__(self):
+        return self.memberFuncName
 
-    def tri(self, specs):
-        pass
+    def trap(self,input):
+        specs = self.specs
+        return max(min( (input-specs[0]) / (specs[1] - specs[0]), 1 ,(specs[3] - input)/(specs[3] - specs[2]) ),0)
 
-
-def tri(specs,input):
-    return max(min( (input-specs[0]) / (specs[1] - specs[0]), (specs[2] - input)/(specs[2] - specs[1]) ),0)
-
-def trap(specs,input):
-    return max(min( (input-specs[0]) / (specs[1] - specs[0]), 1 ,(specs[3] - input)/(specs[3] - specs[2]) ),0)
-
+    def tri(self, input):
+        specs = self.specs
+        return max(min( (input-specs[0]) / (specs[1] - specs[0]), (specs[2] - input)/(specs[2] - specs[1]) ),0)
 
 
 def main():
-    print(trap([1,2,3,4],3.5))
+    t = memFunc([1,2,3])
+    print(t.memFunc(2))
 
 
 if __name__ == '__main__':
