d71c9d560a5ae4d1ab48575505f73bb384cbbdf2
Sam Kreter <samkreter@gmail.com>
2016-10-23 15:53:05 -0500

    added operator generating class

---
+++ operators.py
@@ -0,0 +1,25 @@
+
+class Operators():
+
+    def printStuff(self):
+        print(dir(self))
+
+    def getFunc(self,operator):
+        return getattr(self,operator)
+
+    def compliment(self, x):
+        return 1 - x
+
+    def intersection(self, x, y):
+        return min(x,y)
+
+    def union(self, x, y):
+        return max(x,y)
+
+
+def main():
+    t = Operators().getFunc("union")
+    print(t(.5,.4))
+
+if __name__ == '__main__':
+    main()
