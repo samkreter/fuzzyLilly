a3f39fc87faa94357f480e78cf3ca64bb2f92e40
Sam Kreter <samkreter@gmail.com>
2016-10-23 19:40:09 -0500

    changing intersect for multiple args

--- runner.py
+++ runner.py
@@ -2,10 +2,20 @@ from tree import Tree
 import numpy as np
 
 
-my_data = np.genfromtxt('car_data.csv', delimiter=',')
+def convertCarData(data):
+    return [data[5],data[3],data[4],data[6],data[1],data[2]]
+
+
+
+car_data = np.genfromtxt('car_data.csv', delimiter=',')
+
+
 
 
 dTree = Tree('tree1.json')
 
-print(dTree.printInputs())
+for data in car_data:
+    dTree.changeInputs(convertCarData(data))
+    print(dTree.run())
+
 
