c77ab500e73d0e8689ea38e5a58a26fd897ce452
Sam Kreter <samkreter@gmail.com>
2016-10-25 20:37:07 -0500

    added plotmemship func

--- runner.py
+++ runner.py
@@ -1,5 +1,6 @@
 from tree import DecisionTree
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def convertCarData(data):
@@ -11,12 +12,23 @@ car_data = np.genfromtxt('car_data.csv', delimiter=',')
 
 dTree = DecisionTree('tree1.json')
 scores = []
+best = [0,0]
 
 for data in car_data:
     dTree.changeInputs(convertCarData(data))
     score = dTree.run()
+    if(score > best[0]):
+    	best[0] = score
+    	best[1] = data
     scores.append(score)
     print(score)
+
+print(best[0])
+print(best[1])
+
+n, bins, patches = plt.hist(scores, normed=1, facecolor='green', alpha=0.75)
+plt.show()
+
 
 
 
