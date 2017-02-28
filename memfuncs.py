a522b59e0ce45dff7981f455c4e425d333f84c10
Sam Kreter <samkreter@gmail.com>
2017-02-28 07:18:51 -0600

    added first round of fuzzy membership function

--- memfuncs.py
+++ memfuncs.py
@@ -1,13 +1,20 @@
 import numpy as np
 
+
+fsize = 10
+
 class MemFunc():
     """Creates the specific membership function and is used to continue the process"""
 
-    def __init__(self, name,specs=[]):
-        length = len(specs)
+    def __init__(self, name,specs=[],fMemFunc = False, stage0Op = False):
+
         #Get the specs of how to define the membership functions
         self.specs = specs
         self.memFuncName = name
+
+        if fMemFunc and stage0Op:
+            self.fMemFunc = getattr(self,fMemFunc)
+            self.stage0Op = stage0Op
 
         self.memFunc = getattr(self,name)
 
@@ -16,6 +23,33 @@ class MemFunc():
     # when trying to print
     def __repr__(self):
         return self.memberFuncName
+
+
+    def fuzzyMem(self,input):
+
+        #Create a fuzzy number from the number that is passed in
+        fNum = MemFunc("tri",[input - fsize / 2, input, (input + fsize / 2 )])
+
+        #Collet the new fuzzy set thats created
+        newFSet = []
+
+        #That nice trick to be able to quicly add operators
+        operators = {"mul": np.multiply,
+                     "min": min}
+
+
+        #Convert the operator string to the actual op
+        # Weird thing to do but its a little safer than just letting the user
+        # pass in a random op, at least for now
+        op = operators[self.stage0Op]
+
+
+        #Iterate throught the set
+        for i in range(input - fsize // 2, (input + fsize // 2 ) + 1):
+            newFSet.append((op(fNum.memFunc(i),self.fMemFunc(i)),i))
+
+        return newFSet
+
 
 
     def gauss(self,input):
